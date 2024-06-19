1f59b684c4743638ddb51b61b9d2287a934ee2ac
silviaalemany <silviamalemany@gmail.com>
2024-06-19 15:15:39 -0700

    adding 2.3

--- org/Fund.java
+++ org/Fund.java
@@ -1,20 +1,34 @@
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Fund {
-
+	public class CountDonations {
+		public int count;
+		public long sum;
+		public CountDonations() {
+			this.count = 0;
+			this.sum = 0;
+		}
+		public void addDonation(long amount) {
+			this.count += 1;
+			this.sum += amount;
+		}
+	}
 	private String id;
 	private String name;
 	private String description;
 	private long target;
 	private List<Donation> donations;
-	
+	private Map<String, CountDonations> aggregates;
 	public Fund(String id, String name, String description, long target) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.target = target;
 		donations = new LinkedList<>();
+		aggregates = new HashMap<>();
 	}
 
 	public String getId() {
@@ -35,12 +49,20 @@ public class Fund {
 
 	public void setDonations(List<Donation> donations) {
 		this.donations = donations;
+		for (Donation d : donations) {
+			CountDonations cd = aggregates.getOrDefault(d.getContributorName(), new CountDonations());
+			cd.addDonation(d.getAmount());
+			aggregates.put(d.getContributorName(), cd);
+		}
 	}
 	
 	public List<Donation> getDonations() {
 		return donations;
 	}
 	
+	public Map<String, CountDonations> getDonationAggregates() {
+		return aggregates;
+	}
 	
 	
 }
--- org/UserInterface.java
+++ org/UserInterface.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
-
 public class UserInterface {
 	
 	
@@ -44,7 +47,29 @@ public class UserInterface {
                 if (option == 0) {
                     createFund();
                 } else if (option <= org.getFunds().size() && option > 0) {
-                    displayFund(option);
+                    OUTER:
+                    while (true) {
+                        System.out.println("Enter 'a' if you want to see the full list of contributions");
+                        System.out.println("Enter 'b' if you want to see contributions aggregated by contributor");
+                        System.out.println("Or enter 'q' or 'quit' to exit");
+                        choice = in.nextLine().trim();
+                        switch (choice) {
+                            case "quit", "q" -> {
+                                System.out.println("Good bye!");
+                                break OUTER;
+                            }
+                            case "a" -> {
+                                displayFund(option);
+                                break OUTER;
+                            }
+                            case "b" -> {
+                                displayFundAggregates(option);
+                                break OUTER;
+                            }
+                            default -> System.out.println("That wasn't an option, try again");
+                        }
+                    }
+
                 } else {
                     System.out.println("Fund number is out of bounds.");
                 }
@@ -126,6 +151,33 @@ public class UserInterface {
         in.nextLine();
         
     }
+	public void displayFundAggregates(int fundNumber) {
+        
+        Fund fund = org.getFunds().get(fundNumber - 1);
+        
+        System.out.println("\n\n");
+        System.out.println("Here is information about this fund:");
+        System.out.println("Name: " + fund.getName());
+        System.out.println("Description: " + fund.getDescription());
+        System.out.println("Target: $" + fund.getTarget());
+        
+        Map<String, Fund.CountDonations> aggregates = fund.getDonationAggregates();
+        System.out.println("Number of contributors: " + aggregates.size());
+        double total = 0;
+        List<Entry<String, Fund.CountDonations>> ordering = new ArrayList<>(aggregates.entrySet());
+        Collections.sort(ordering, (Entry<String, Fund.CountDonations> o1, Entry<String, Fund.CountDonations> o2) -> (int)(o2.getValue().sum - o1.getValue().sum));
+        for (Entry<String, Fund.CountDonations> aggregate : ordering) {
+            System.out.println("* " + aggregate.getKey() + ", " + aggregate.getValue().count + " donations, $" + aggregate.getValue().sum + " total");
+            total += aggregate.getValue().sum;
+        }
+         
+        double percent = (total / fund.getTarget()) * 100;
+        System.out.printf("Total donation amount: $%.2f (%.2f%% of target)\n", total, percent);
+        
+        System.out.println("Press the Enter key to go back to the listing of funds");
+        in.nextLine();
+        
+    }
 	
 	
 	public static void main(String[] args) {
@@ -139,7 +191,7 @@ public class UserInterface {
 		try{
 			org = ds.attemptLogin(login, password);
 
-		} catch(IllegalStateException e){
+		} catch(Exception e){
 			System.out.println("Error communicating with the server.");
 			return;
 		}
@@ -155,5 +207,7 @@ public class UserInterface {
 		
 		}
 	}
+
+	
 
 }
